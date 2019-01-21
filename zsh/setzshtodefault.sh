64ee96d8671ad971aa52485028eda9fcf5e7ba71
Raymond <Ray2729@outlook.com>
2019-01-21 20:21:54 +0800

    Update setzshtodefault.sh

--- zsh/setzshtodefault.sh
+++ zsh/setzshtodefault.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 [ ! $(id -u) == "0" ] || { echo "ERROR: You should't be root"; exit 1; }
 chsh -s /bin/zsh
-wget https://github.com/zsh-users/antigen/releases/download/v2.2.3/antigen.zsh
-mv antigen.zsh "$HOME"/.antigen.zsh
+mv .antigen.zsh "$HOME"/.antigen.zsh
 mv .zshrc  "$HOME"/
